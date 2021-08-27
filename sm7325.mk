97c79201e482b7deda43bdb0fe5096d54718b200
kubersharma001 <kubersharma001@gmail.com>
2021-08-27 17:25:10 +0000

    sm7325-common: Import powerhint.json from coral
    * From: https://android.googlesource.com/device/google/coral/+/android-11.0.0_r42/powerhint.json
    
    Change-Id: I8dba2912afb27e0432bdce601ce38f2934bb0186
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- sm7325.mk
+++ sm7325.mk
@@ -388,6 +388,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.moto-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
