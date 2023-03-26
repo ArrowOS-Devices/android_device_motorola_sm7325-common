1ad508678fb91359df24f3d78806dac874c6d80e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-03-26 17:43:57 +0530

    sm7325-common: Disable rounded corners in HWC
    Taken from stock. We do not use hardware masking for rounded corners.

--- rootdir/bin/init.qti.display_boot.sh
+++ rootdir/bin/init.qti.display_boot.sh
@@ -111,9 +111,9 @@ case "$target" in
         setprop vendor.display.enable_perf_hint_large_comp_cycle 1
         setprop vendor.display.enable_qsync_idle 1
         setprop vendor.display.enable_allow_idle_fallback 1
-        setprop vendor.display.enable_rounded_corner 1
-        setprop vendor.display.disable_rounded_corner_thread 0
-        setprop vendor.display.enable_rc_support 1
+        #setprop vendor.display.enable_rounded_corner 1
+        #setprop vendor.display.disable_rounded_corner_thread 0
+        #setprop vendor.display.enable_rc_support 1
         setprop vendor.display.enable_hdr10_gpu_target 1
         ;;
     esac
